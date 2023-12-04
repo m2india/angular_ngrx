a4e6e7a527e6386f1f6722be27603318a191f29c
Manimaran <manimaraan.r@gmail.com>
2023-12-04 20:08:17 +0530

    ngrx devtool added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -14,6 +14,7 @@ import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 
 @NgModule({
@@ -31,7 +32,15 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    StoreModule.forRoot({ counter: counterReducer })
+    StoreModule.forRoot({ counter: counterReducer }),
+    StoreDevtoolsModule.instrument({
+      // maxAge: 25, // Retains last 25 states
+      logOnly: isDevMode(), // Restrict extension to log-only mode
+      // autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      // trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      // traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+      // connectInZone: true // If set to true, the connection is established within the Angular zone
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
